2fdadb670eeea84be6007776278ff3fbdefeced3
linkeusblind <linkeusblind@gmail.com>
2020-01-22 19:34:43 +0100

    add css to assistant

--- voice-assist/app.js
+++ voice-assist/app.js
@@ -1,10 +1,12 @@
 const btn = document.querySelector('.talk');
-const content = document.querySelector('.content');
+const content = document.querySelector('.answer');
 
 // Odpowiedzi
 const greetings = ['Spoko ziomek, tragedii nie ma', 'A co cię to obchodzi', 'Najlepszy dzień ever', 'Całkiem dobrze, a co u Ciebie', 'Zostaw mnie w spokoju'];
 
-const weather = ['Jutro będzie piękny dzień', 'Pogoda będzie szałowa oooooiiii', 'Będzie lało jak nie wiem co, lepiej zostań w domu', 'To będzie dobry dzień na gorącą czekoladę i kocyk', 'Daj spokój, szkoda gadać']
+const weather = ['Jutro będzie piękny dzień', 'Pogoda będzie szałowa oooooiiii', 'Będzie lało jak nie wiem co, lepiej zostań w domu', 'To będzie dobry dzień na gorącą czekoladę i kocyk', 'Daj spokój, szkoda gadać'];
+
+const dobranoc = ['Dobranoc, życzę Ci miłych snów', 'a a a kotki dwa, a ja mówię pa pa pa', 'Kiedy piękna noc zapada. Księżyc bajki opowiada. Wszystkie gwiazdy go słuchają Bo na piękny sen czekają. Więc bez żadnych zbędnych słów. Życzę Tobie miłych snów', 'Gwiazdka głośno z nieba krzyczy, Dobrej nocy Tobie życzy.']
 //
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -39,10 +41,14 @@ function readOutLoud(message){
         const finalText = weather[Math.floor(Math.random() * weather.length)];
         speech.text = finalText;
     }
+    if(message.includes('dobranoc')){
+        const finalText = dobranoc[Math.floor(Math.random() * dobranoc.length)];
+        speech.text = finalText;
+    }
 
     speech.volume = 1;
     speech.rate = 1;
-    speech.pitch = 1;
+    speech.pitch = 0.7;
 
     window.speechSynthesis.speak(speech);
 
